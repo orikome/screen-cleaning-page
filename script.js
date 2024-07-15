e50d9b09f49e72e1f5a3382111fabab07bed1f61
orikome <wyrplen@gmail.com>
2024-07-15 15:25:41 +0900

    Return boxes back to original shape after full screen

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.querySelectorAll('.color-box').forEach(box => {
-    box.addEventListener('click', () => document.fullscreenElement ? document.exitFullscreen() : box.requestFullscreen());
+document.querySelectorAll(".color-box").forEach((box) => {
+  box.addEventListener("click", () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen().then(() => {
+        box.style.borderRadius = "33.3%";
+      });
+    } else {
+      box.requestFullscreen().then(() => {
+        box.style.borderRadius = "0";
+      });
+    }
+  });
+});
+// When exiting full screen, return back to squircle
+document.addEventListener("fullscreenchange", () => {
+  if (!document.fullscreenElement) {
+    document.querySelectorAll(".color-box").forEach((box) => {
+      box.style.borderRadius = "33.3%";
+    });
+  }
 });
